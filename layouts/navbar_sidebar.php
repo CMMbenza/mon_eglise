70d0e7b5606ffa499c365ee85dca3cc9b7d49651
unknown <cm.chrismbenza@gmail.com>
2026-06-14 00:22:25 +0200

    masquage du lien boostrap js dans le deux navbar admin et user

--- layouts/navbar_sidebar.php
+++ layouts/navbar_sidebar.php
@@ -293,4 +293,4 @@ if ($base_url === '') {
 
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
--- layouts/navbar_sidebar_fideles.php
+++ layouts/navbar_sidebar_fideles.php
@@ -222,3 +222,5 @@ if ($base_url === '') {
     </div>
 
 </div>
+
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
